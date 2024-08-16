3a8a1b2033aab3f416a2c4117ed190d47a7b7a9c
int43 <Natthapat.int43@gmail.com>
2024-08-16 16:50:03 +0700

    main

--- beginner/beginner/demo/demo/src/main/java/com/excellence/demo/controller/order/request/ExampleOrderRequest.java
+++ beginner/beginner/demo/demo/src/main/java/com/excellence/demo/controller/order/request/ExampleOrderRequest.java
@@ -1,13 +1,28 @@
+//package is used to determine the file location.
 package com.excellence.demo.controller.order.request;
 
+//import is used for importing code from other files. So that the code is divided and stored in separate files.
 import com.excellence.demo.model.ExampleOrder;
 import com.excellence.demo.model.OrderStatus;
 import com.excellence.demo.model.ValidateResult;
 
-import java.time.LocalDate;
+/*
+import java.time.LocalDate; Imports the LocalDate class from the java.time package, represents a date without time and timezone information.
+import java.time.format.DateTimeFormatter;  Imports the LocalDate class from the java.time package, used for formatting and parsing dates and times in a locale-sensitive manner.
+import java.util.Date;      Imports the Date class from the java.util package, represents a specific instant in time, with millisecond precision.
+*/
+import java.time.LocalDate; 
 import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
+/*
+declare class "ExampleOrderRequest"
+    declare "itemId" in "Integer" that the value of this variable can't be changed after the initial configuration has been received.
+    declare "name" in "String" that the value of this variable can't be changed after the initial configuration has been received.
+    declare "amount" in "Integer" that the value of this variable can't be changed after the initial configuration has been received.
+    declare "orderStatus" in "String" that the value of this variable can't be changed after the initial configuration has been received.
+    declare "orderDate" in "String" that the value of this variable can't be changed after the initial configuration has been received.
+*/
 public class ExampleOrderRequest {
     public final Integer itemId;
     public final String name;
@@ -15,6 +30,15 @@ public class ExampleOrderRequest {
     public final String orderStatus;
     public final String orderDate;
 
+    /*
+    declare "validate()" when implemented, would perform some sort of validation process and return a "ValidateResult".
+    if "itemId < 1", ValidateResult will return the response fail with the message "itemId can't be less than 1"
+    if "name" is empty(""), ValidateResult will return the response fail with the message ""name can't be empty""
+    if "amount < 1", ValidateResult will return the response fail with the message "amount can't be less than 1"
+    if "orderStatus" isn't valid, ValidateResult will return the response fail with the message "orderStatus: PENDING, COMPLETED, CANCELLED are only allowed"
+    if "orderDate" is in the past, ValidateResult will return the response fail with the message "orderDate can't be in the past"
+
+    */
     public ValidateResult validate() {
         if (itemId < 1) return ValidateResult.failed("itemId can't be less than 1");
 
--- beginner/beginner/demo/demo/src/main/java/com/excellence/demo/datasource/OrderDatasource.java
+++ beginner/beginner/demo/demo/src/main/java/com/excellence/demo/datasource/OrderDatasource.java
@@ -45,13 +45,11 @@ public class OrderDatasource implements OrderRepository {
     @Override use to declare the method below. It works instead of a method that looks the same in the class that invokes this method.
     declare "getAllOrder()" to get all datas from List<ExampleOrder>
     declare "sql" query to select all columns from the example_order table.
-    List<Map<String, Object>> records = jdbcTemplate.queryForList(sql); This line of code runs a SQL query and stores the results in a list.
-        jdbcTemplate: An object that helps run SQL queries.
-        queryForList(sql): Runs the SQL query and returns the results.
-        List<Map<String, Object>>: A list where each item is a map (like a dictionary) with column names as keys and their corresponding values.
-    records.stream() Turns the list of records into a stream.
-        .map(record -> toModel(record)) Converts each record into a model using the "toModel" method.
-        .collect(toList()) Collects all the models into a list.
+    List<Map<String, Object>> records = jdbcTemplate.queryForList(sql); declare list name "records" by each set of data can store two values, namely string and object, 
+        and let records get the value from the database using the JdbcTemplate class, call the queryForList method to get the list value<Maps> using the SQL command from the sql string.
+    records.stream() : Turns the list of records into a stream.
+        .map(record -> toModel(record)) : Converts each record into a model using the "toModel" method.
+        .collect(toList()) : Collects all the models into a list and returns it.
     */
     @Override
     public List<ExampleOrder> getAllOrder() {
@@ -82,6 +80,13 @@ public class OrderDatasource implements OrderRepository {
                 entity.orderDate);
     }
 
+    /*
+    @Override use to declare the method below. It works instead of a method that looks the same in the class that invokes this method.
+    declare "updateOrder()" use parameter "order" of "ExampleOrder"
+    declare "entity" into ExampleOrderEntity that converts the order object into an ExampleOrderEntity object.
+    defines the sql to update a new row into the example_order table.
+    runs the SQL query with the values from the entity object to update the order into the database.
+    */
     @Override
     public void updateOrder(ExampleOrder order) {
         ExampleOrderEntity entity = ExampleOrderEntity.of(order);
@@ -97,6 +102,14 @@ public class OrderDatasource implements OrderRepository {
         );
     }
 
+    /*
+    @Override use to declare the method below. It works instead of a method that looks the same in the class that invokes this method.
+    declare "getOrder()" to get the data from ExampleOrder by use parameter int id.  
+    defines a SQL query that selects all columns from the example_order table where the id column matches the provided id.
+    The jdbcTemplate is used to execute the SQL query. It replaces the ? in the SQL with the provided id and returns a list of maps, where each map represents a row from the result set.
+    If the query result is empty (meaning no record was found with that id), it returns an empty ExampleOrder object.
+    If there is at least one record, it takes the first one (since id should be unique, there should only be one) and converts it into an ExampleOrder object using the toModel method.
+    */
     @Override
     public ExampleOrder getOrder(int id) {
         String sql = "SELECT * FROM example_order WHERE id = ?";
@@ -105,12 +118,29 @@ public class OrderDatasource implements OrderRepository {
         return toModel(records.get(0));
     }
 
+    /*
+    @Override use to declare the method below. It works instead of a method that looks the same in the class that invokes this method.
+    declare "deleteOrder()" use parameter "(int id)"
+    defines the sql to delete a data from the example_order table where the id matches the provided parameter.
+    Runs the SQL query the ? in the query is replaced by the value of id instance of JdbcTemplate.
+    */
     @Override
     public void deleteOrder(int id) {
         String sql = "DELETE FROM example_order WHERE id = ?";
         jdbcTemplate.update(sql, id);
     }
 
+    /*
+    declare "toModel" that use parameter "record" which is map two values, namely string and object, to convert the record into "ExampleOrder".
+    declare "date" that get the data from "record" which associated with key "order_date" to a "Date" type.
+    return instance of ExampleOrder class.
+        send the "record" value with the key "id" to int.
+        send the "record" value with the key "item_id" to int.
+        send the "record" value with the key "name" to string.
+        send the "record" value with the key "amount" to int.
+        send the value with the key "order_status" to string and convert it to the enum value of "OrderStatus"
+        convert "Date" to "LocalDate".
+    */
     private ExampleOrder toModel(Map<String, Object> record) {
         Date date = (Date) record.get("order_date");
         return new ExampleOrder(
